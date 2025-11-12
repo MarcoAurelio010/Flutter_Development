5dce5dc08ce4e26d16a70ebb90f1287ac1e72d40
Marco Aurélio Nichi Rodermel <marcoaurelionichi@gmail.com>
2025-11-12 17:17:58 -0300

    Delete flutter_atividade_9_geolocalizacao directory

--- flutter_atividade_9_geolocalizacao/README.md
+++
@@ -1,3 +0,0 @@
-# flutter_atividade_9_geolocalizacao
-
-A new Flutter project.
--- flutter_atividade_9_geolocalizacao/android/app/src/main/kotlin/com/example/flutter_atividade_9_geolocalizacao/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_atividade_9_geolocalizacao
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
